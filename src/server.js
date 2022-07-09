b68e73ee1d35f413da791e3f3b4af206a780edce
Aarish Khanna <68593746+AarishKhanna@users.noreply.github.com>
2022-07-09 20:55:36 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 //JSON file for deployed contract and network information
-const mainContractJSON = require('src/build/contracts/MainContract.json')
-const electionJSON = require('src/build/contracts/Election.json')
+const mainContractJSON = require('/MainContract.json')
+const electionJSON = require('/Election.json')
 
 require("dotenv").config();
 
