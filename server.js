1bab7575afd99aa816a065b73dd5b79116e80bbe
Aarish Khanna <68593746+AarishKhanna@users.noreply.github.com>
2022-07-11 00:20:47 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+//JSON file for deployed contract and network information
+const mainContractJSON = require('/src/build/contracts/MainContract.json')
+const electionJSON = require('/src/build/contracts/Election.json')
+
+require("dotenv").config();
+
+app.use(express.static("./"));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+//Sending MainContract JSON file for its interaction using Truffle
+app.get('/mainContractJSON', (req, res) => {
+    res.send(mainContractJSON);
+});
+
+//Sending ABI object directly for Election contract, since only ABI will be used
+app.get('/electionJSON', (req, res) => {
+    res.send(electionJSON.abi);
+});
